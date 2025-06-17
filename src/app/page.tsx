df3f34c98a650e742f9d855dadd11cd12cb45749
AeroNyx <130458686+AeroNyxNetwork@users.noreply.github.com>
2025-06-18 04:12:54 +0900

    Update page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,15 +1,17 @@
 import { Navbar } from '@/components/layout/Navbar'
 import { HeroSection } from '@/components/sections/HeroSection'
 import { GoldStandardSection } from '@/components/sections/GoldStandardSection'
+import { PixelBackground } from '@/components/effects/PixelBackground'
 
 export default function Home() {
   return (
     <>
+      <PixelBackground />
       <Navbar />
-      <main>
+      <main className="relative z-10">
         <HeroSection />
         <GoldStandardSection />
-        {/* Additional sections will be added here */}
+        {/* 更多部分可以继续添加 */}
       </main>
     </>
   )
