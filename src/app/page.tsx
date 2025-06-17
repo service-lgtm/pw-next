cd1d74653035a895cae51decbf0e16f9a35ddbe0
AeroNyx <130458686+AeroNyxNetwork@users.noreply.github.com>
2025-06-18 04:49:50 +0900

    Update page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,6 +2,7 @@ import { Navbar } from '@/components/layout/Navbar'
 import { HeroSection } from '@/components/sections/HeroSection'
 import { GoldStandardSection } from '@/components/sections/GoldStandardSection'
 import { NFTAssetsSection } from '@/components/sections/NFTAssetsSection'
+import { MiningSystemSection } from '@/components/sections/MiningSystemSection'
 import { ProsumerSection } from '@/components/sections/ProsumerSection'
 import { PixelBackground } from '@/components/effects/PixelBackground'
 
@@ -14,6 +15,7 @@ export default function Home() {
         <HeroSection />
         <GoldStandardSection />
         <NFTAssetsSection />
+        <MiningSystemSection />
         <ProsumerSection />
         {/* 更多部分可以继续添加 */}
       </main>
