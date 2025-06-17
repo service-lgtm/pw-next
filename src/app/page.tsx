89c4091db90c6a3d5a3e4eae8086ce0df458c8d9
AeroNyx <130458686+AeroNyxNetwork@users.noreply.github.com>
2025-06-18 05:39:28 +0900

    Update page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -6,6 +6,7 @@ import { NFTAssetsSection } from '@/components/sections/NFTAssetsSection'
 import { MiningSystemSection } from '@/components/sections/MiningSystemSection'
 import { MarketplaceSection } from '@/components/sections/MarketplaceSection'
 import { ProsumerSection } from '@/components/sections/ProsumerSection'
+import { RankingSystemSection } from '@/components/sections/RankingSystemSection'
 import { OnboardingSection } from '@/components/sections/OnboardingSection'
 import { PixelBackground } from '@/components/effects/PixelBackground'
 
@@ -22,6 +23,7 @@ export default function Home() {
         <MiningSystemSection />
         <MarketplaceSection />
         <ProsumerSection />
+        <RankingSystemSection />
         <OnboardingSection />
         {/* 更多部分可以继续添加 */}
       </main>
