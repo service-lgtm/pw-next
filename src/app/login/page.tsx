61c17af5c8f89f2f49367875ce399f22a9284dad
AeroNyx <130458686+AeroNyxNetwork@users.noreply.github.com>
2025-07-09 05:21:26 +0800

    Update page.tsx

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -1,18 +1,5 @@
-// import { AuthPage } from '@/components/auth/AuthComponents'
-
-// export default function LoginPage() {
-//   return <AuthPage type="login" />
-// }
-// 在 login/page.tsx 中
 import { AuthPage } from '@/components/auth/AuthComponents'
-import { TestLoginForm } from '@/components/TestLoginForm'
 
 export default function LoginPage() {
-  return (
-    <>
-      <AuthPage type="login" />
-      {/* 临时添加测试组件 */}
-      {process.env.NODE_ENV === 'development' && <TestLoginForm />}
-    </>
-  )
+  return <AuthPage type="login" />
 }
