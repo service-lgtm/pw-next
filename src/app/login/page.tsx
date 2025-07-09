119a94072cbd0d0fac79080b3e1962966550e098
AeroNyx <130458686+AeroNyxNetwork@users.noreply.github.com>
2025-07-09 15:08:11 +0800

    Update page.tsx

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -1,11 +1,5 @@
 import { AuthPage } from '@/components/auth/AuthComponents'
-import DebugNetwork from '@/components/DebugNetwork'
 
 export default function LoginPage() {
-  return (
-    <>
-      <AuthPage type="login" />
-      {process.env.NODE_ENV === 'development' && <DebugNetwork />}
-    </>
-  )
+  return <AuthPage type="login" />
 }
